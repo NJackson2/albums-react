1ca78e153b4a2a7b1ccb4919ff9aa74d68a3da46
Nick Jackson <nickdjackson@msn.com>
2022-11-01 17:09:52 -0400

    fourth

--- src/components/AddAlbum.jsx
+++ src/components/AddAlbum.jsx
@@ -4,25 +4,25 @@ import { useState } from "react"
 export default function AddAlbum({setToggle, toggle}) {   //pulling in a function from the parent
     const [album, setAlbum] = useState('')  //use this state variable to control this component
     const [artist, setArtist] = useState('') 
-    const [year, setYear] = useState(1970) 
-    const handleSubmit = (e) => {        //put in album data and it does this fetch
-        e.preventDefault()
+    const [year, setYear] = useState(1970) //this is just a placeholder
+    const handleSubmit = (e) => {        //put in album data and it does this fetch. Takes an event as a parameter.
+        e.preventDefault()   // have to add this line to prevent HTML from its default behavior - put this on every form there is
         //let's check to see that they entered all the data
-        if(!album || !artist || !year) {
+        if(!album || !artist || !year) {  //this code should never run (just a backup) - we added required to input belows
             alert('Please enter all info')
             return
         }
-        const newAlbum = {artist, album, year}
-        fetch('https://albums-api-nj.web.app/albums', {
+        const newAlbum = {artist, album, year}     //putting this together into a single object
+        fetch('https://albums-api-nj.web.app/albums', {   //for anything other than a get request (like a post), we need to put in options. Some of these (headers and body) are done automatically by postman. 
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(newAlbum)
+            body: JSON.stringify(newAlbum)    // we compress the object to a single string. JSON parse takes it back to a regular JS object.
         })
             .then(() => {
-                //assume it worked...
-                setToggle(!toggle) // set it to the opposite of the toggle
+                //if I get request, then assume it worked (this is why we did not add the other .then re: JSON)
+                setToggle(!toggle) // set it to the opposite of the toggle. This is sent up to APP and down to toggle list in AlbumList.
                 setAlbum('')
                 setArtist('')
                 setYear('')
@@ -33,10 +33,12 @@ export default function AddAlbum({setToggle, toggle}) {   //pulling in a functio
         <section className = "add-album">
             <h3>Add new Album</h3>
             <form onSubmit={handleSubmit}>
+                {/* form starts here and has the below three elements. When we submit the form, it  */}
                 <label htmlFor="album">Album:
                     <input type="text" name="album" required
                     onChange = {e => setAlbum(e.target.value)} 
-                    value={album}/>                
+                    value={album}/>
+                    {/*  */}
                 </label><br /><br />
                 <label htmlFor="artist">Artist:
                     <input type="text" name="artist" required
--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 import AlbumCard from "../AlbumCard";
 
-export default function AlbumList({toggle}) {
-    const [albums, setAlbums] = useState()
-    useEffect(() => {
-        fetch('https://albums-api-nj.web.app/albums')
+export default function AlbumList({toggle}) {   //get the toggle/prop from APP
+    const [albums, setAlbums] = useState()     //state variable because we want react to respond to it
+    useEffect(() => {    // want to put the fetch in a useEffect so that the fetch does not continually reload 
+        fetch('https://albums-api-nj.web.app/albums') //basic fetch get request
             .then(response => response.json())
             .then(setAlbums)
             .catch(alert)
-    }, [toggle])  //empty [] means it will only load once/call the API once (it is an empty dependency list)
+    }, [toggle])  //empty [] means it will only load once/call the API once (it is an empty dependency list). Originally we only had it run once - but now with toggle, whenever toggle is triggered, we run it again
 
         return (
         <main className="album-list">
